e7258e3f3d63864f987bf8f61e8099b6f11faff0
JeeWook Kim <jeewook@google.com>
2018-09-02 13:37:43 +0900

    GAMPCSV enhance

--- S2STest/src/jw/demo/GAMPCSV.java
+++ S2STest/src/jw/demo/GAMPCSV.java
@@ -7,6 +7,7 @@ package jw.demo;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
@@ -38,51 +39,58 @@ public class GAMPCSV {
 		options.addOption("i", "input", true, "csv input file path");
 		options.addOption("c", "column", true, "cid column name");
 		options.addOption("p", "property", true, "GA property tracking ID");
-		options.addOption("ec", "category", false, "event category");
-		options.addOption("ea", "action", false, "event action");
-		options.addOption("cd", "dimension", false, "custom dimension index");
-		options.addOption("v", "value", false, "custom dimension value");
+		options.addOption("e", "category", true, "event category");
+		options.addOption("a", "action", true, "event action");
+		options.addOption("d", "dimension", true, "custom dimension index");
+		options.addOption("v", "value", true, "custom dimension value");
 		
         HelpFormatter formatter = new HelpFormatter();
-        String filepath = "";
-        String cidcolumn = "";
-        String property = "";
+        String filepath = null;
+        String cidcolumn = null;
+        String property = null;
         String eventCategory = null;
         String eventAction = null;
         String cdindex = null;
         String cdvalue = null;
-        
+        int count =1;
         try {
         	CommandLine line = clparser.parse( options, args );
-            filepath = line.getOptionValue("input");
-            cidcolumn = line.getOptionValue("column");
-            property = line.getOptionValue("property");
+        	if( line.hasOption( "input" ) ) {
+        		filepath = line.getOptionValue("input");
+        		System.out.println("filepath="+filepath);        		
+        	} else throw new Exception("no value for argument --input");
+        	if( line.hasOption( "column" ) ) {
+        		cidcolumn = line.getOptionValue("column");
+        		System.out.println("cidcolumn="+cidcolumn);
+        		
+        	} else throw new Exception("no value for argument --column");
+        	if( line.hasOption( "property" ) ) {
+        		property = line.getOptionValue("property");
+        		System.out.println("property="+property);
+        	} else throw new Exception("no value for argument --property");
             if( line.hasOption( "category" ) ) {
             	eventCategory = line.getOptionValue("category"); 
-            }
+            	System.out.println("eventCategory="+eventCategory);
+            } else throw new Exception("no value for argument --category");
             if( line.hasOption( "action" ) ) {
             	eventAction = line.getOptionValue("action"); 
-            }
+            	System.out.println("eventAction="+eventAction);
+            } else throw new Exception("no value for argument --action");
             if( line.hasOption( "dimension" ) ) {
             	cdindex = line.getOptionValue("dimension"); 
+            	System.out.println("cdindex="+cdindex);
             }
             if( line.hasOption( "value" ) ) {
             	cdvalue = line.getOptionValue("value"); 
+            	System.out.println("cdvalue="+cdvalue);
             }
-        } catch (ParseException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("GAMPCSV", options);
+            formatter.printHelp("java GAMPCSV", options);
             System.exit(1);
         }
 
-        
-        System.out.println(filepath);
-        System.out.println(cidcolumn);
-        System.out.println(property);
-
         CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
-
-		// initialize the CSVParser object
 		CSVParser parser;
 		URI uri;
 		CloseableHttpClient httpclient = HttpClients.createDefault();
@@ -103,17 +111,16 @@ public class GAMPCSV {
 			throw e1;
 		}
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-
+		Date start = new Date();
+		System.out.println("start time="+start.toString());
 		for (CSVRecord record : parser) {
 			params.clear();
 			params.add(new BasicNameValuePair("v", "1"));
 			params.add(new BasicNameValuePair("tid", property));
 			params.add(new BasicNameValuePair("cid", record.get(cidcolumn)));
 			params.add(new BasicNameValuePair("t", "event"));
-			if (eventCategory != null)
-				params.add(new BasicNameValuePair("ec", eventCategory));
-			if (eventAction != null)
-				params.add(new BasicNameValuePair("ea", eventAction));
+			params.add(new BasicNameValuePair("ec", eventCategory));
+			params.add(new BasicNameValuePair("ea", eventAction));
 			if (cdindex != null && cdvalue != null)
 				params.add(new BasicNameValuePair("cd"+cdindex, cdvalue));			
 			params.add(new BasicNameValuePair("ni", "1"));
@@ -123,6 +130,7 @@ public class GAMPCSV {
 						.setHost("www.google-analytics.com")
 						.setPath("/collect").build(); 
 				HttpPost httppost = new HttpPost(uri);
+				System.out.println("count="+count);
 				System.out.println(httppost.getURI());
 				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
 				System.out.println(entity.toString());
@@ -132,11 +140,17 @@ public class GAMPCSV {
 				CloseableHttpResponse response = httpclient.execute(httppost);
 				System.out.println(response.getStatusLine());
 				response.close();
-			} catch (URISyntaxException e) {
+				count ++;
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
-		}
+		} // for
+		Date end = new Date();
+		System.out.println("end time="+end.toString());
+		long seconds = (end.getTime()-start.getTime())/1000;
+		System.out.println("duration(sec)="+seconds);
+		System.out.println("requests/sec="+count/seconds);
 		// close the parser
 		parser.close();
 
